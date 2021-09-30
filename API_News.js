b9c1035db1e5ef510be6066ab931421b954c7cde
marvin.zeiter <marvin.zieter@suisa.ch>
2021-09-30 14:04:05 +0200

    yes

--- API_News.js
+++
@@ -1,8 +0,0 @@
-var url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=1c22439bbd4448c889748fc80d548b8d';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
